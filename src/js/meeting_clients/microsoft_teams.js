c2137dfabb751c11a971cbe0790628de4b7fd2b8
Karan Toor <currentoor@gmail.com>
2022-04-25 10:58:58 -0700

    Added additional teams url

--- src/js/meeting_clients/microsoft_teams.js
+++ src/js/meeting_clients/microsoft_teams.js
@@ -1,59 +1,58 @@
-import {getElement} from '../utils.js';
-import {Clients} from './clients.js';
-
+import { getElement } from "../utils.js";
+import { Clients } from "./clients.js";
 
 export class MicrosoftTeams extends Clients {
-    constructor() {
-        super();
-        this.targetUrls = ["teams.microsoft.com", "teams.live.com"];
+  constructor() {
+    super();
+    this.targetUrls = [
+      "teams.microsoft.com",
+      "teams.live.com",
+      "dod.teams.microsoft.us",
+    ];
+  }
+
+  getStatus() {
+    let status = "";
+    if (getElement('[aria-label="Unmute"]')) {
+      status += "chromeMute:muted,";
+    } else if (getElement('[aria-label="Mute"]')) {
+      status += "chromeMute:unmuted,";
+    } else {
+      status += "chromeMute:disabled,";
     }
 
-    getStatus() {
-        let status = ''
-        if(getElement('[aria-label="Unmute"]')){
-            status += 'chromeMute:muted,';
-        }else if(getElement('[aria-label="Mute"]')){
-            status += 'chromeMute:unmuted,';
-        }else{
-            status += "chromeMute:disabled,"
-        }
-        
-        
-        if(getElement('[aria-label="Turn camera on"]')){
-            status += 'chromeVideo:stopped,';
-        }else if(getElement('[aria-label="Turn camera off"]')){
-            status += 'chromeVideo:started,';
-        }else{
-            status += "chromeVideo:disabled,"
-        }
-
-        return status;
+    if (getElement('[aria-label="Turn camera on"]')) {
+      status += "chromeVideo:stopped,";
+    } else if (getElement('[aria-label="Turn camera off"]')) {
+      status += "chromeVideo:started,";
+    } else {
+      status += "chromeVideo:disabled,";
     }
 
-    toggleMute() {
-        let muteButton = (
-            getElement('[aria-label="Unmute"]') ||
-            getElement('[aria-label="Mute"]')
-        );
-    
-        if(muteButton){
-            muteButton.click()
-        }else{
-            console.error("Could not find google meet's mute button")
-        }
-    }
+    return status;
+  }
 
-    toggleVideo() {
-        let videoButton = (
-            getElement('[aria-label=\"Turn camera on\"]') ||
-            getElement('[aria-label=\"Turn camera off\"]')
-        );
-    
-        if(videoButton){
-            videoButton.click()
-        }else{
-            console.error("Could not find google meet's video button")
-        }
-        return
+  toggleMute() {
+    let muteButton =
+      getElement('[aria-label="Unmute"]') || getElement('[aria-label="Mute"]');
+
+    if (muteButton) {
+      muteButton.click();
+    } else {
+      console.error("Could not find google meet's mute button");
     }
+  }
+
+  toggleVideo() {
+    let videoButton =
+      getElement('[aria-label="Turn camera on"]') ||
+      getElement('[aria-label="Turn camera off"]');
+
+    if (videoButton) {
+      videoButton.click();
+    } else {
+      console.error("Could not find google meet's video button");
+    }
+    return;
+  }
 }
